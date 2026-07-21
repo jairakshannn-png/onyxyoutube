a7f3cb84f88f861eb4d73e6071814185c7517988
jairakshannn-png <jairakshannn@gmail.com>
2026-07-21 11:40:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { Innertube, UniversalCache } from 'youtubei.js';Unsupported Media Type
+import { Innertube, UniversalCache } from 'youtubei.js';
